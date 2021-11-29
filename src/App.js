702ab59030ac6880d52d2a2035dda72fc3a05bc8
Daniel Deverell <daniel.deverell@gmail.com>
2021-11-29 08:57:47 -0600

    new day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import useFetch from "./hooks/useFetch";
+import useToggle from "./hooks/useToggle";
+
+import Recipes from "./Recipes";
+import RecipeDetail from "./RecipeDetail";
+import Nav from "./Nav";
 
 function App() {
+  const [loggedin, setLoggedin] = useToggle(true);
+  const [recipes, setRecipes] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+  const { get, post, del } = useFetch(`/api/recipes`);
+
+  /* eslint-disable react-hooks/exhaustive-deps */
+  React.useEffect(() => {
+    setLoading(true);
+    get("/api/recipes")
+      .then((data) => {
+        setRecipes(data);
+        setLoading(false);
+      })
+      .catch((error) => setError(error));
+  }, []);
+
+  const addRecipe = (recipe) => {
+    post("/api/recipes", recipe).then((data) => {
+      setRecipes([data, ...recipes]);
+    });
+  };
+
+  const deleteRecipe = (recipeId) => {
+    console.log("recipeId:", recipeId);
+    del(`/api/recipes/${recipeId}`)
+      .then(
+        setRecipes((recipes) =>
+          recipes.filter((recipe) => recipe._id !== recipe.recipeId)
+        )
+      )
+      .then(
+        get("/api/recipes").then((data) => {
+          setRecipes(data);
+        })
+      );
+  };
+
+  if (loading === true) {
+    return <p>Loading</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <BrowserRouter>
+        <Nav setLoggedin={setLoggedin} loggedin={loggedin} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Recipes
+                recipes={recipes}
+                loggedin={loggedin}
+                addRecipe={addRecipe}
+              />
+            }
+          />
+          <Route
+            path="/:recipeId"
+            element={
+              <RecipeDetail
+                recipes={recipes}
+                deleteRecipe={deleteRecipe}
+                loggedin={loggedin}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
 
---
+++ src/RecipeDetail.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+
+function RecipeDetail({ recipes, loggedin, deleteRecipe }) {
+  const { recipeId } = useParams();
+  const [recipeDeleted, setRecipeDeleted] = React.useState(false);
+
+  const currRecipe = recipes.filter((recipe) => recipe._id === recipeId);
+  const thisRecipe = { ...currRecipe[0] };
+
+  const delRecipe = () => {
+    deleteRecipe(thisRecipe._id);
+    setRecipeDeleted(true);
+  };
+
+  if (recipeDeleted) {
+    return (
+      <>
+        <p>Recipe deleted!</p>
+        <Link to="/">Home</Link>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <img src={`/img/${thisRecipe.image}`} alt={thisRecipe.title} />
+      <h1>{thisRecipe.title}</h1>
+      <p>{thisRecipe.description}</p>
+
+      {loggedin && <button onClick={() => delRecipe()}>delete</button>}
+
+      <Link to="/">Home</Link>
+    </div>
+  );
+}
+
+export default RecipeDetail;
